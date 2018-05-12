d49a56236c485cdccc77025663364023437f1726
yangholmes <yangholmes@126.com>
2018-05-12 21:44:06 +0800

    [新增]新增Express文件服务器不需要再以Nginx的目录运行项目, 完善了READMA.md

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Express文件服务器
+ * @file
+ * @author Yangholmes
+ */
+const express = require('express');
+const app = express();
+
+app.use(express.static('./'));
+
+let server = app.listen(8978, function () {
+    let host = server.address().address;
+    let port = server.address().port;
+
+    console.log('App listening at http://%s:%s', host, port);
+});
