f3a9565098a3e0d1ea0524ad781d39b5b0ec1735
yangholmes <yangholmes@126.com>
2024-08-20 01:24:24 +0800

    chore: 备份旧版本，更新新的静态生成工具

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo first, build the blog
-call npm run blog:build
-
-@echo second, remove all files and folders in ./blog
-del .\blog\* /Q
-
-@echo third, move dist into blog directory
-xcopy .\blog-dev\.vuepress\dist\* .\blog /E /Y
--- build.sh
+++
@@ -1,7 +0,0 @@
-# first, build the blog
-npm run blog:build
-
-# second, remove all files and folders in ./blog
-
-# third, move dist into blog directory
-cp -f -r ./blog-dev/.vuepress/dist/* ./blog
